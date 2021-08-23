6484a7637818ba177866fb86f3dd51fbd16d1df0
vivienneobrien <vxo910@cs.bham.ac.uk>
2021-08-23 15:31:17 +0100

    Fixed toggle

---
+++ script.js
@@ -0,0 +1,19 @@
+// // document.body.innerHTML = '<h1>yooo</h1>'
+
+let yearly = false;
+
+document
+  .getElementById("toggle-checkbox")
+  .addEventListener("click", function checked() {
+    yearly = !yearly;
+    const prices = document.getElementsByClassName("price");
+    const pricesDuration = document.getElementsByClassName("price-duration");
+    for (let i = 0; i < prices.length; i++) {
+      const price = prices[i];
+      price.innerText = yearly ? price.dataset.yearly : price.dataset.monthly;
+      const priceDuration = pricesDuration[i];
+      priceDuration.innerText = yearly
+        ? priceDuration.dataset.yearly
+        : priceDuration.dataset.monthly;
+    }
+  });
